3f62071326e9c59b4e0ea3f89e929ca2725ff432
AmrSultan5 <amrsultan505@gmail.com>
2024-11-28 04:12:55 +0200

    connecting to db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CoursesModule],
+  imports: [  
+    ConfigModule.forRoot(), 
+    MongooseModule.forRoot(process.env.Mongo),
+    CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
